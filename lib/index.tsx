07bfa8258740653d1c7a39bfab7be14b84780410
脱安琪 <453001363@qq.com>
2020-05-28 23:55:46 +0800

    feat: index.d.ts文件移到dist目录里了

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 import Button from './button';
 
-console.log(Button, 'button======');
-ReactDOM.render(<Button></Button>, document.body)
+const a = 1;
+export {Button, a};
